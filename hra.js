438ce87b8eb161f00b264733a5585388ed51e975
Kristýna <kristyna.adlofova@gmail.com>
2023-04-10 23:11:49 +0200

    addClass function modification

--- hra.js
+++ hra.js
@@ -1,17 +1,33 @@
-let currentPlayer = 'circle'
+let currentPlayer = 'circle';
+const playerElm = document.querySelector('#player');
 
-const addClassCircle = (event) =>  {
-    event.target.classList.add('board__field--circle')
+const addClass = (event) => {
+    if (currentPlayer==='circle') {
+    currentPlayer = 'cross';
+    event.target.classList.add('board__field--circle');
+    event.target.disabled = true;  
+    playerElm.classList.remove('board__field--circle');    
+    return playerElm.classList.add('board__field--cross');    
+}
+  
+if (currentPlayer==='cross') {
+    currentPlayer = 'circle';
+    event.target.classList.add('board__field--cross');
+    event.target.disabled = true
+    playerElm.classList.remove('board__field--cross');    
+    return playerElm.classList.add('board__field--circle');
+    }
 }
 
-document.querySelector('button:nth-child(1)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(2)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(3)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(4)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(5)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(6)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(7)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(8)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(9)').addEventListener('click', addClassCircle);
-document.querySelector('button:nth-child(10)').addEventListener('click', addClassCircle);
-
+document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(2)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(3)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(4)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(5)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(6)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(7)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(8)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(9)').addEventListener('click', addClass);
+document.querySelector('button:nth-child(10)').addEventListener('click', addClass);

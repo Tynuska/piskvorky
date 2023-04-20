116e56b05361688592ddecc25969c2b912a41137
Kristýna <kristyna.adlofova@gmail.com>
2023-04-20 13:01:40 +0200

    document.querySelectorAll for buttons

--- hra.js
+++ hra.js
@@ -1,33 +1,45 @@
-let currentPlayer = 'circle';
-const playerElm = document.querySelector('#player');
+let currentPlayer = "circle";
+const playerElm = document.querySelector("#player");
 
 const addClass = (event) => {
-    if (currentPlayer==='circle') {
-    currentPlayer = 'cross';
-    event.target.classList.add('board__field--circle');
-    event.target.disabled = true;  
-    playerElm.classList.remove('board__field--circle');    
-    return playerElm.classList.add('board__field--cross');    
-}
-  
-if (currentPlayer==='cross') {
-    currentPlayer = 'circle';
-    event.target.classList.add('board__field--cross');
-    event.target.disabled = true
-    playerElm.classList.remove('board__field--cross');    
-    return playerElm.classList.add('board__field--circle');
-    }
-}
+  if (currentPlayer === "circle") {
+    event.target.classList.add("board__field--circle");
+    event.target.disabled = true;
+    playerElm.classList.add("board__field--cross");
+    playerElm.classList.remove("board__field--circle");
+    return (currentPlayer = "cross");
+  }
 
-document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(2)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(3)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(4)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(5)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(6)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(7)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(8)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(9)').addEventListener('click', addClass);
-document.querySelector('button:nth-child(10)').addEventListener('click', addClass);
+  if (currentPlayer === "cross") {
+    event.target.classList.add("board__field--cross");
+    event.target.disabled = true;
+    playerElm.classList.remove("board__field--cross");
+    playerElm.classList.add("board__field--circle");
+    return (currentPlayer = "circle");
+  }
+};
+
+// //proč nefunguje?
+// const buttons = document.querySelector('.field')
+// const buttonsAll = buttons.querySelectorAll('button')
+// buttonsAll.forEach((button) => {
+//     button.addEventListener('click,', addClass)
+// })
+
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", addClass);
+  console.log(button);
+});
+
+// document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(1)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(2)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(3)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(4)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(5)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(6)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(7)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(8)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(9)').addEventListener('click', addClass);
+// document.querySelector('button:nth-child(10)').addEventListener('click', addClass);
